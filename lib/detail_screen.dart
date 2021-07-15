10977667d6a885f97d1fbe53157299fe61e4b6ce
nurcity <nurbol.sakenov@outlook.com>
2021-07-15 20:00:59 +0600

    detail screen wip

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -58,15 +58,39 @@ class DetailScreen extends StatelessWidget {
                       SessionCard(
                         sessionNum: 1,
                         isDone: true,
+                        press: () {},
                       ),
                       ...List.generate(
                         5,
                         (index) => SessionCard(
                           sessionNum: index + 1,
                           isDone: false,
+                          press: () {},
                         ),
                       )
                     ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Mediation',
+                    style: Theme.of(context).textTheme.headline6!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(13),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor)
+                      ],
+                    ),
                   )
                 ],
               ),
@@ -82,53 +106,60 @@ class SessionCard extends StatelessWidget {
   SessionCard({
     required this.sessionNum,
     this.isDone = false,
+    required this.press,
   });
 
   final int sessionNum;
   final bool isDone;
+  final VoidCallback press;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraint) {
       return Container(
         width: constraint.maxWidth / 2 - 5,
-        padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(13),
-            boxShadow: [
-              BoxShadow(
-                offset: Offset(0, 17),
-                blurRadius: 23,
-                spreadRadius: -13,
-                color: kShadowColor,
-              )
-            ]),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(13),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 17),
+              blurRadius: 23,
+              spreadRadius: -13,
+              color: kShadowColor,
+            )
+          ],
+        ),
         child: Material(
-          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(13),
+          shadowColor: kShadowColor,
+          clipBehavior: Clip.hardEdge,
           child: InkWell(
-            onTap: () {},
-            child: Row(
-              children: [
-                Container(
-                  height: 42,
-                  width: 43,
-                  decoration: BoxDecoration(
-                    color: isDone ? kBlueColor : Colors.white,
-                    shape: BoxShape.circle,
-                    border: Border.all(color: kBlueColor),
+            onTap: press,
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Container(
+                    height: 42,
+                    width: 43,
+                    decoration: BoxDecoration(
+                      color: isDone ? kBlueColor : Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: kBlueColor),
+                    ),
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: isDone ? Colors.white : kBlueColor,
+                    ),
                   ),
-                  child: Icon(
-                    Icons.play_arrow,
-                    color: isDone ? Colors.white : kBlueColor,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'Session $sessionNum',
-                  style: Theme.of(context).textTheme.subtitle2,
-                )
-              ],
+                  SizedBox(width: 10),
+                  Text(
+                    'Session $sessionNum',
+                    style: Theme.of(context).textTheme.subtitle2,
+                  )
+                ],
+              ),
             ),
           ),
         ),
